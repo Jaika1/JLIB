23928725f598c59962a16cb0072435115070850e
Jack R <jackroennfeldtnospam@gmail.com>
2021-11-08 15:02:44 +1030

    Added SharedRandom

---
+++ JLIB/Utils/Random/SharedRandom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JLIB.Utils.Random
+{
+    /// <summary>
+    /// Contains a thread-safe way of obtaining an instance of <c>System.Random</c>, great for when you want a global random where new instantiation is unnessecary or messy.
+    /// </summary>
+    /// <remarks>If targeting .NET 6.0 or greater, consider using <c>System.Random.Shared</c> instead.</remarks>
+#if NET6_0_OR_GREATER
+    [Obsolete("This class is no longer required as of .NET 6.0 due to the introduction of the System.Random.Shared property.")]
+#endif
+    public static class SharedRandom
+    {
+        private static ThreadLocal<System.Random> _random = new ThreadLocal<System.Random>();
+
+        /// <summary>
+        /// Provides a thread-safe instance of <c>System.Random</c>.
+        /// </summary>
+        public static System.Random Instance => _random.Value;
+    }
+}
